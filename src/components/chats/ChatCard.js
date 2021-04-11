8b86d30e80d6ff70f83fbfe1c327d637eccc9662
jmiohc <54696956+CoRoRo10@users.noreply.github.com>
2021-04-11 18:19:12 +0900

    최신소스 반영

--- src/components/chats/ChatCard.js
+++ src/components/chats/ChatCard.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import FaceIcon from '@material-ui/icons/Face';
 
 const ChatCard = React.memo(({chat, users, index, uid, onEmojiClick}) => {
   const who = () => {
@@ -6,59 +7,54 @@ const ChatCard = React.memo(({chat, users, index, uid, onEmojiClick}) => {
     if (user){
       return user.nickName
     }else{
-      return "noname"
+      return "anonymous"
     }
   } 
 
-  const renderEmojiSection = () => {
-    const emojiObj = chat.emoji
-    return <>
-      <div className="fdr">
-        <EmojiButton emojiKey={'1'} emojiValue={"😎"} emojiObj={emojiObj}/>
-        <EmojiButton emojiKey={'2'} emojiValue={"🤣"} emojiObj={emojiObj}/>
-      </div>  
-    </>
-  }
+  // const renderEmojiSection = () => {
+  //   const emojiObj = chat.emoji
+  //   return <>
+  //     <div className="fdr">
+  //       <EmojiButton emojiKey={'1'} emojiValue={"😎"} emojiObj={emojiObj}/>
+  //       <EmojiButton emojiKey={'2'} emojiValue={"🤣"} emojiObj={emojiObj}/>
+  //     </div>  
+  //   </>
+  // }
 
-  const EmojiButton = ({emojiKey, emojiValue, emojiObj}) => {
-    let extraClassName = ""
-    if(emojiObj && emojiObj[emojiKey]){
-      if(emojiObj[emojiKey].includes(uid)){
-        extraClassName = "active"
-      }
-    }
+  // const EmojiButton = ({emojiKey, emojiValue, emojiObj}) => {
+  //   let extraClassName = ""
+  //   if(emojiObj && emojiObj[emojiKey]){
+  //     if(emojiObj[emojiKey].includes(uid)){
+  //       extraClassName = "active"
+  //     }
+  //   }
 
-    return <div>
-      <div onClick={e=> onEmojiClick(emojiKey, chat.id)} className={`emojiRec flex fdr aic ${extraClassName}`}>
-        <span>{emojiValue}</span>
-        <span>
-          {emojiObj && emojiObj[emojiKey] &&
-            <div>{emojiObj[emojiKey].length}</div>
-          }
-        </span>
-      </div>
-    </div>
-  }
+  //   return <div>
+  //     <div onClick={e=> onEmojiClick(emojiKey, chat.id)} className={`emojiRec flex fdr aic ${extraClassName}`}>
+  //       <span>{emojiValue}</span>
+  //       <span>
+  //         {emojiObj && emojiObj[emojiKey] &&
+  //           <div>{emojiObj[emojiKey].length}</div>
+  //         }
+  //       </span>
+  //     </div>
+  //   </div>
+  // }
 
   return <div className="flex fdr pb16 chat_card" key={index}>
     <div className="w40 h40 flex aic jcc">
-      <img src="http://www.gravatar.com/avatar/3b3be63a4c2a439b013787725dfce802?d=identicon"
-        className="w30 h30"/>
+      <FaceIcon color='primary' fontSize='large'/>
     </div>
-    <div className="pl16 f1">
+    <div className="pl16 f1" style={{backgroundColor:'#dee5f4', borderRadius:20, marginLeft:10}}>
       <div>
-        <span className="bold">{who()}</span>
-        <span className="fs color_gray pl8">-?</span>
+        <span className="bold" >{who()}</span>
       </div>
       <div className="pt8">
         {chat.content}
       </div>
-      {renderEmojiSection()}
     </div>    
   </div>
 }, (prevProps, nextProps) => {
-  // console.log('chat card');
-  // console.log((prevProps.chat === nextProps.chat) && (prevProps.users === nextProps.users))
   return (prevProps.chat === nextProps.chat) && (prevProps.users === nextProps.users)
 });
 
