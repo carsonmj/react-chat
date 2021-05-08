66b789ec579ec609e0a38b8a66197ce957509f65
mj <eunoia2040@gmail.com>
2021-05-09 02:54:29 +0900

    in progress login page css

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../App.css";
+import "../login.css";
 import { useHistory } from "react-router-dom";
 import { firebaseApp } from "../firebase";
 import Spinner from "../components/Spinner";
@@ -56,6 +56,29 @@ const Login = () => {
   const [initLoaded, setInitLoaded] = useState(false);
   const [loading, setLoading] = useState(false);
   const [loginStatus, setLoginStatus] = useState(false);
+
+  const [loginFlag, setLoginFlag] = useState(true);
+  const [signupFlag, setSignupFlag] = useState(false);
+
+  const switchers = [...document.querySelectorAll('.switcher')]
+
+  switchers.forEach(item => {
+    item.addEventListener('click', function() {
+      switchers.forEach(item => item.parentElement.classList.remove('is-active'))
+      this.parentElement.classList.add('is-active')
+    })
+  })
+
+  const onClickSwitcherLogin = () => {
+    setSignupFlag(false);
+    setLoginFlag(true);
+  }
+
+  const onClickSwitcherSignup = () => {
+    setLoginFlag(false);
+    setSignupFlag(true);
+  }
+
 
   const login = () => {
     if (email.length < 3) {
@@ -129,14 +152,65 @@ const Login = () => {
 
   return (
     <div>
-      <Spinner show={loading} />
+      {/* <Spinner show={loading} /> */}
       {loginStatus ? (
         <>
           <div className="btn btn-danger" onClick={(evt) => {logout();}}> logout </div>
         </>
       ) : (
         <>
-          <Container component="main" maxWidth="xs">
+        <section class="forms-section">
+          <h1 class="section-title">Let's Chat!</h1>
+          <div class="forms">
+            <div class={loginFlag? "form-wrapper is-active" : "form-wrapper"} >
+                  <button type="button" class="switcher switcher-login" onClick={onClickSwitcherLogin}>
+                    Login
+                    <span class="underline"></span>
+                  </button>
+                  <form class="form form-login">
+                        <fieldset>
+                          <legend>Please, enter your email and password for login.</legend>
+                          <div class="input-block">
+                            <label for="login-email">E-mail</label>
+                            <input id="login-email" type="email" required />
+                          </div>
+                          <div class="input-block">
+                            <label for="login-password">Password</label>
+                            <input id="login-password" type="password" required />
+                          </div>
+                        </fieldset>
+                        <button type="submit" class="btn-login">Login</button>
+                  </form>
+            </div>
+            <div class={signupFlag? "form-wrapper is-active" : "form-wrapper"}>
+              <button type="button" class="switcher switcher-signup" onClick={onClickSwitcherSignup}>
+                Sign Up
+                <span class="underline"></span>
+              </button>
+              <form class="form form-signup">
+                <fieldset>
+                  <legend>Please, enter your email, password and password confirmation for sign up.</legend>
+                  <div class="input-block">
+                    <label for="signup-email">E-mail</label>
+                    <input id="signup-email" type="email" required />
+                  </div>
+                  <div class="input-block">
+                    <label for="signup-password">Password</label>
+                    <input id="signup-password" type="password" required />
+                  </div>
+                  <div class="input-block">
+                    <label for="signup-password-confirm">Confirm password</label>
+                    <input id="signup-password-confirm" type="password" required />
+                  </div>
+                </fieldset>
+                <button type="submit" class="btn-signup">Continue</button>
+              </form>
+            </div>
+          </div>
+        </section>
+
+
+          {/* <Container component="main" maxWidth="xs">
             <CssBaseline />
             <div className={classes.paper}>
               <form className={classes.form} noValidate>
@@ -206,7 +280,7 @@ const Login = () => {
             </div>
             <Box mt={8}>
             </Box>
-          </Container>
+          </Container> */}
         </>
       )}
     </div>
